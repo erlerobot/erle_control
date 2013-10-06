faa444b05c0479abfecb992e7ab5136ee9f4e114
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2013-10-06 16:21:12 +0200

    imu class squeleton added

--- imu.py
+++ imu.py
@@ -10,7 +10,33 @@
 '''
 
 class IMU:
-    """ Interface with the Inertial Measurement Unit
+    """ Interface with the Inertial Measurement Unit. 
+    
+    The IMU consists of an InvenSense 9-axis MPU-9150. This sensor provides
+    readings from 3 accelerometers, 3 magnetometers and 3 gyroscopes.
+    Furthermore, the module has a DMP (Digital Motion Processor) integrated
+    that makes the calculations necessary to provide filtered outputs.
     """
+    def __init__(self):
+        #TODO Set I2C interface, make sure that calibrations files are available and make some readings
+        # through ctypes.
+        pass
 
+    """ Reads the data from the I2C interface (50 Hz max).
+    """
+    def i2cRead(self):
+        #TODO implement
+        pass
+
+    """ Reads data from the FIFO list (250 Hz max?).
+    """
+    def fifoRead(self):
+        #TODO implement
+        pass
+
+    """ Reads raw data (withouth using DMP).
+    """
+    def rawRead(self):
+        #TODO implement
+        pass
 
