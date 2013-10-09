5f05f6d6ae28b5b7de9acf4fb078e6c4cd1ba54b
vmayoral <v.mayoralv@gmail.com>
2013-10-09 20:06:36 +0200

    imu.py: sensor initialization provided.
    A first attemp to reuse the read functions from
    the C code has been done however it seems there's
    some sort of trouble with it. C functions will be
    coded that return the information as a parameter
    passed by reference.

--- imu.py
+++ imu.py
@@ -9,6 +9,7 @@
 @author: Víctor Mayoral Vilches <victor@erlerobot.com>
 @description: python interface with the MPU-9150.
 '''
+from ctypes import *
 
 class IMU:
     """ Interface with the Inertial Measurement Unit. 
@@ -21,14 +22,37 @@ class IMU:
     def __init__(self):
         #TODO Set I2C interface, make sure that calibrations files are available and make some readings
         # through ctypes.
-        pass
+        self.lib = CDLL("./imu/libimu.so")
+        self.i2c_bus = 2
+        self.lib.mpu9150_set_debug(0) # 1
+        self.sample_rate = 50 # 50 Hz
+        self.yaw_mix_factor = 3
 
-    """ Reads the data from the I2C interface (50 Hz max).
+        # initialize the IMU
+        res = self.lib.mpu9150_init(i2c_bus, sample_rate, yaw_mix_factor)
+        if res:
+            Exception("Error when initializing the IMU!")
+        # set calibration files
+        res = self.lib.set_cal(0, "./imu/accelcal.txt")
+        if res != 0:
+            Exception("Error while calibration: accelcal.txt")
+        res = self.lib.set_cal(0, "./imu/magcal.txt")
+        if res != 0:
+            Exception("Error while calibration: magcal.txt")
+
+
+    """ Reads the fused data from the sensor
         @return roll, pitch, yaw
     """
-    def i2cRead(self):
-        #TODO implement
-        pass
+    def fusedRead(self):
+        """
+        # Code for passing a parameter directly to a C function
+        # however it doesn't seem to work. Some debuggin might be neccesary
+        mpu = Mpudata_t()
+        self.lib.mpu9150_read(addressof(mpu))
+        """
+        #TODO create C functions that return
+
 
     """ Reads data from the FIFO list (250 Hz max?).
     """
@@ -42,3 +66,27 @@ class IMU:
         #TODO implement
         pass
 
+    def calibration(self):
+        pass
+
+"""
+# array class
+Vector3d_t = 3*c_float
+# array class
+Quaternion_t = 4*c_float
+# struct 
+class Mpudata_t(Structure):
+         _fields_ = [("rawGyro", c_short*3),
+                     ("rawAccel", c_short*3),
+                     ("rawQuat", c_long*4),
+                     ("dmpTimestamp", c_ulong*4),
+                     ("rawMag", c_short*4),
+                     ("magTimestamp", c_ulong*4),
+                     ("calibratedAccel", c_short*4),
+                     ("calibratedMag", c_short*4),
+                     ("fusedQuat", Quaternion_t),
+                     ("fusedEuler", Vector3d_t),
+                     ("lastDMPYaw", c_float),
+                     ("lastYaw", c_float)
+                    ]
+"""
